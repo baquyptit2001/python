99c9462e30ecf4b5afe32ee321b4f8e65323c4f2
baquyptit2001 <quynb201@gmail.com>
2021-08-26 22:10:55 +0700

    python

--- lam-tron-so_PY01003.py
+++ lam-tron-so_PY01003.py
@@ -1,10 +1,19 @@
 t = int(input())
-
-for u in range(t):
-    if (int(number) < 10):
-        print(number)
-        continue
-    number = input()
-    tmp = list(number)
-    for i in range(len(tmp) - 1, 0, - 1):
-
+while t > 0:
+    s = input()
+    s = s[::-1]
+    l = len(s)
+    tmp = []
+    for i in range(l):
+        tmp.append(s[i])
+    if l == 1:
+        print(s)
+    else:
+        for i in range(len(tmp) - 1):
+            if int(tmp[i]) >= 5:
+                tmp[i] = '0'
+                tmp[i + 1] = str(int(tmp[i + 1]) + 1)
+            else:
+                tmp[i] = '0'
+        print(''.join(tmp[::-1]))
+    t -= 1
